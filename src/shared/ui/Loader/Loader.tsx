e577f1702ac175e3d42fb7e4b97db6adad0c485f
Komandr33 <and.kom2017@gmail.com>
2026-03-28 21:56:24 +0300

    feat: add Loader

---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,18 @@
+import s from './Loader.module.scss'
+
+type LoaderProps = {
+  children?: React.ReactNode
+}
+
+export const Loader = ({ children }: LoaderProps) => {
+  return (
+    <div className={s.overlay}>
+      <div className={s.contentLoader}>
+        <span className={s.loader}></span>
+        <p className={s.childrenContent}>{children}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Loader
