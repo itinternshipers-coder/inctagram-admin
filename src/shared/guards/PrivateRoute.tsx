4e9cdca7878bff4141141a7afb494ce502d68dcd
itinternshipers-coder <itinternshipers@gmail.com>
2026-03-31 22:01:17 +0300

    Fix-hydration-mismatch (#8)
    * chore: remove pnpm-workspace.yaml (not needed for single project) (#1)
    
    * chore: add files devops-settings (#3)
    
    * add docks (#5)
    
    * chore: add files devops-settings
    
    * chore: initial deploy commit
    
    * chore: add dock git-workflow
    
    * chore: add script "build:production"
    
    * "fix: resolve hydration mismatch in PrivateRoute by moving token check to useEffect" (#7)
    
    ---------
    
    Co-authored-by: AndreyKomarov <and.kom2017@gmail.com>

--- src/shared/guards/PrivateRoute.tsx
+++ src/shared/guards/PrivateRoute.tsx
@@ -1,20 +1,23 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 import Loader from '@/shared/ui/Loader/Loader'
 
 export function PrivateRoute({ children }: { children: React.ReactNode }) {
   const router = useRouter()
-  const token = typeof window !== 'undefined' ? localStorage.getItem('adminToken') : null
+  const [isAuthorized, setIsAuthorized] = useState<boolean | null>(null)
 
   useEffect(() => {
+    const token = localStorage.getItem('adminToken')
     if (!token) {
       router.replace('/sign-in')
+    } else {
+      setIsAuthorized(true) // eslint-disable-line react-hooks/set-state-in-effect
     }
-  }, [router, token])
+  }, [router])
 
-  if (!token) {
+  if (isAuthorized === null) {
     return <Loader />
   }
 
