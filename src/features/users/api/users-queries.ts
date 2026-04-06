dc608fc994bcd6527f9214e726135cf4b0fd94e8
Artur <metallist15431543@gmail.com>
2026-04-06 18:53:28 +0300

    RC-225 feat: add GraphQL queries for user details, payments and uploaded photos

--- src/features/users/api/users-queries.ts
+++ src/features/users/api/users-queries.ts
@@ -21,3 +21,59 @@ export const GET_USERS = gql`
     }
   }
 `
+
+export const GET_USER = gql`
+  query GetUser($id: String!) {
+    user(id: $id) {
+      id
+      username
+      profileLink
+      email
+      isBanned
+      banReason
+      bannedAt
+      createdAt
+      avatarUrl
+      avatarSmallUrl
+    }
+  }
+`
+
+export const GET_USER_PAYMENTS = gql`
+  query GetUserPayments($input: UserPaymentsInput!) {
+    userPayments(input: $input) {
+      items {
+        id
+        amount
+        date
+        paymentMethod
+        subscription
+        userId
+        username
+        avatarUrl
+      }
+      page
+      pageSize
+      totalCount
+      totalPages
+    }
+  }
+`
+
+export const GET_POSTS = gql`
+  query GetPosts($input: PostsListInput) {
+    posts(input: $input) {
+      items {
+        id
+        username
+        photos {
+          id
+          url
+          order
+        }
+      }
+      hasMore
+      nextCursor
+    }
+  }
+`
