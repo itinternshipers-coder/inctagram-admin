d41f3deeb235dbe7646af5dc5c17f1dc2190ec6f
Artur <metallist15431543@gmail.com>
2026-04-06 18:55:54 +0300

    RC-225 feat: add navigation from users table to user details page

--- src/features/users/ui/UsersTable.tsx
+++ src/features/users/ui/UsersTable.tsx
@@ -5,6 +5,7 @@ import s from './UsersTable.module.scss'
 import { Typography } from '@/shared/ui/Typography/Typography'
 import BlockIcon from '@/shared/icons/BlockIcon'
 import MoreHorizontalOutlineIcon from '@/shared/icons/MoreHorizontalOutlineIcon'
+import Link from 'next/link'
 
 type UsersTableProps = {
   users: User[]
@@ -75,9 +76,13 @@ export function UsersTable({ users, sortDirection, onSort }: UsersTableProps) {
                 </Typography>
               </td>
               <td className={s.td}>
-                <button className={s.moreButton}>
+                <Link
+                  href={`/users/${user.id}`}
+                  className={s.moreButton}
+                  aria-label={`More information about ${user.username}`}
+                >
                   <MoreHorizontalOutlineIcon size={20} />
-                </button>
+                </Link>
               </td>
             </tr>
           ))}
