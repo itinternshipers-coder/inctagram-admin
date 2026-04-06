ecd834323d36be913a3cef56f41930939b93309f
Artur <metallist15431543@gmail.com>
2026-04-06 18:39:17 +0300

    RC-225 feat: switch user details to real backend data and remove mock usage

--- src/app/(dashboard)/users/[userId]/page.tsx
+++ src/app/(dashboard)/users/[userId]/page.tsx
@@ -1,4 +1,3 @@
-import { mockUsers } from '@/features/users/model/mock-data'
 import { UserDetailsView } from '@/features/users/ui/user-details/UserDetailsView'
 
 type UserPageProps = {
@@ -7,7 +6,6 @@ type UserPageProps = {
 
 export default async function UserPage({ params }: UserPageProps) {
   const { userId } = await params
-  const user = mockUsers.find((item) => item.id === userId) ?? mockUsers[0]
 
-  return <UserDetailsView user={user} requestedUserId={userId} />
+  return <UserDetailsView requestedUserId={userId} />
 }
