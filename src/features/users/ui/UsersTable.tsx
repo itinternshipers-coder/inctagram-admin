d0f885e1c066210ef25e0b1ce6687cea8acc4645
NovikovRomanV <rimm19845@gmail.com>
2026-04-19 13:25:28 +0400

    fix: UsersTable.tsx; UsersTable.module.scss; Loader.tsx; Loader.module.scss

--- src/features/users/ui/UsersTable.tsx
+++ src/features/users/ui/UsersTable.tsx
@@ -1,16 +1,20 @@
 'use client'
 
-import { User, SortDirection } from '../model/types/types'
+import { User } from '../model/types'
 import s from './UsersTable.module.scss'
 import { Typography } from '@/shared/ui/Typography/Typography'
 import BlockIcon from '@/shared/icons/BlockIcon'
 import MoreHorizontalOutlineIcon from '@/shared/icons/MoreHorizontalOutlineIcon'
 import { Table, TableBody, TableDataCell, TableHead, TableHeaderCell, TableRow } from '@/shared/ui'
+import { useState } from 'react'
+import { AgreementsType, PopUpSettingUser } from '@/shared/ui/PopUpSettingUser/PopUpSettingUser'
+import { DirectionType, SortButton, SortBy } from '@/shared/ui/SortButton/SortButton'
+import Loader from '@/shared/ui/Loader/Loader'
 
 type UsersTableProps = {
   users: User[]
-  sortDirection: SortDirection
-  onSort: () => void
+  isLoading: boolean
+  sortDirectionHandlerAction: (direction: DirectionType, type: SortBy) => void
 }
 
 function formatDate(dateStr: string) {
@@ -18,11 +22,23 @@ function formatDate(dateStr: string) {
   return date.toLocaleDateString('ru-RU', { day: '2-digit', month: '2-digit', year: 'numeric' })
 }
 
-export function UsersTable({ users, sortDirection, onSort }: UsersTableProps) {
-  const sortArrow = sortDirection === 'asc' ? ' ↑' : sortDirection === 'desc' ? ' ↓' : ''
+export function UsersTable({ users, isLoading, sortDirectionHandlerAction }: UsersTableProps) {
+  const [openedUserId, setOpenedUserId] = useState<string | null>(null)
+
+  const handleChangeModal = (userId: string | null) => {
+    setOpenedUserId(userId)
+  }
+
+  const modalHandler = (_value: boolean) => {
+    handleChangeModal(null)
+  }
+  const handleOpenAgreementModal = (_value: boolean, _type: AgreementsType) => {
+    setOpenedUserId(null)
+  }
 
   return (
     <div className={s.tableWrapper}>
+      {isLoading && <Loader fullscreen={false}>Loading users...</Loader>}
       <Table className={s.table}>
         <TableHead>
           <TableRow>
@@ -31,9 +47,10 @@ export function UsersTable({ users, sortDirection, onSort }: UsersTableProps) {
                 User ID
               </Typography>
             </TableHeaderCell>
-            <TableHeaderCell className={`${s.th} ${s.sortable}`} onClick={onSort}>
-              <Typography variant="bold_text_14" as="span">
-                Profile link{sortArrow}
+            <TableHeaderCell className={`${s.th} ${s.sortable}`}>
+              <Typography variant="bold_text_14" as="div" className={s.typography}>
+                Profile link
+                <SortButton typeofSort={'userName'} changeDirectionCallback={sortDirectionHandlerAction} />
               </Typography>
             </TableHeaderCell>
             <TableHeaderCell className={s.th}>
@@ -41,20 +58,21 @@ export function UsersTable({ users, sortDirection, onSort }: UsersTableProps) {
                 Username
               </Typography>
             </TableHeaderCell>
-            <TableHeaderCell className={s.th}>
-              <Typography variant="bold_text_14" as="span">
+            <TableHeaderCell className={`${s.th} ${s.sortable}`}>
+              <Typography variant="bold_text_14" as="div" className={s.typography}>
                 Date added
+                <SortButton typeofSort={'created_at'} changeDirectionCallback={sortDirectionHandlerAction} />
               </Typography>
             </TableHeaderCell>
             <TableHeaderCell className={s.th} />
           </TableRow>
         </TableHead>
         <TableBody>
-          {users.map((user, index) => (
-            <TableRow key={index} className={s.row}>
+          {users.map((user) => (
+            <TableRow key={user.id} className={s.row}>
               <TableDataCell className={s.td}>
                 <div className={s.userIdCell}>
-                  {user.isBanned && <BlockIcon size={20} className={s.blockIcon} />}
+                  <div className={s.iconWrapper}>{user.isBanned && <BlockIcon size={20} />}</div>
                   <Typography variant="regular_text_14" as="span">
                     {user.id}
                   </Typography>
@@ -76,9 +94,19 @@ export function UsersTable({ users, sortDirection, onSort }: UsersTableProps) {
                 </Typography>
               </TableDataCell>
               <TableDataCell className={s.td}>
-                <button className={s.moreButton}>
+                <button className={s.moreButton} onClick={() => setOpenedUserId(user.id)}>
                   <MoreHorizontalOutlineIcon size={20} />
                 </button>
+                {openedUserId === user.id && (
+                  <PopUpSettingUser
+                    modalHandler={modalHandler}
+                    userId={user.id}
+                    handleChangeModalAction={() => handleChangeModal(null)}
+                    isOpen={openedUserId === user.id}
+                    handleOpenAgreementModalAction={handleOpenAgreementModal}
+                    userBan={user.isBanned ?? null}
+                  />
+                )}
               </TableDataCell>
             </TableRow>
           ))}
